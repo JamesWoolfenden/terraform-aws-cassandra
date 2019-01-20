059464a8b2f08b5b0e4c88b7738fc7799d119ddf
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-20 18:57:56 +0000

    draft

--- variables.tf
+++ variables.tf
@@ -1,9 +1,20 @@
 variable "region" {
-  default = "eu-west-1"
+  default     = "eu-west-1"
+  description = ""
 }
 
 variable "secondary_region" {
-  default = "us-east-1"
+  description = ""
+  default     = "us-east-1"
 }
 
-variable "instance_type" {}
+variable "instance_type" {
+  description = ""
+}
+
+# this is enables you to add dependancies
+variable depends_on {
+  default     = []
+  description = "This is a way to make a module depends on, which isnt built in."
+  type        = "list"
+}
