0659709b464c46c55f09f7c133e2fe9fc09b6769
James Woolfenden <james.woolfenden@gmail.com>
2019-07-08 22:42:09 +0100

    v0.12 pathway

--- examples/exampleA/module.cassandra.tf
+++
@@ -1,5 +0,0 @@
-module "cassandra" {
-  source        = "../../"
-  instance_type = var.instance_type
-  common_tags   = var.common_tags
-}
--- examples/exampleA/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-instance_type = "t3.micro"
-
-common_tags = {
-"name" = "cassandra"
-"Environment" = "Development" }
