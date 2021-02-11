55e2f1ac81f6e42c8cb0b7cca1fcc0d412c4ebdd
James Woolfenden <james.woolfenden@gmail.com>
2021-02-11 18:29:00 +0000

    workflow

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      version = "3.24.1"
+      version = "3.27.0"
       source  = "hashicorp/aws"
     }
 
@@ -15,4 +15,5 @@ terraform {
       source  = "hashicorp/http"
     }
   }
+  required_version = ">= 0.14"
 }
