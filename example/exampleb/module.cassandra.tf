e9a6c684f3927c770940c2885cf53d5abd94b801
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:28:27 +0100

    chore: pin dependencies to immutable SHAs via ghat

--- example/exampleb/module.cassandra.tf
+++ example/exampleb/module.cassandra.tf
@@ -16,5 +16,5 @@ variable "ami" {
   }
 }
 module "myip" {
-  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=5769331633debca683a81a38470083a0abd39049"
+  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=2f3cef24e667fb840a3d3481f5a1aaa5a1ac7d28" #v0.3.14
 }
